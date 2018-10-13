f7a7a48849ccb1f63351b84625e7759b0ffc1dd6
Pirong <caiseyingzi@gmail.com>
2018-10-13 20:27:36 +0800

    修改代码为面向对象

--- GuiLearn.py
+++ GuiLearn.py
@@ -3,20 +3,27 @@
 from PyQt5.Qt import *
 import sys
 
+# Window类继承QWidget
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # 在类内部调用自己，设置控制
+        self.setWindowTitle("QLabel的学习")
+        self.resize(500, 500)
+
+        # 创建子控件
+        label = QLabel(self)
+        label.setText("xxx")
+
 # 1. 创建一个应用程序对象
 app =  QApplication(sys.argv)
 
 # 2. 控件的操作
-# 2.1 创建控件
-window = QWidget()
-# 2.2 设置控件
-window.setWindowTitle("QLabel的学习")
-window.resize(500, 500)
+# 2.1 通过调用Window类创建控件
+window = Window()
 
-label = QLabel(window)
-label.setText("xxx")
-
-# 2.3 展示控件
+# 2.2 展示控件
 window.show()
 
 # 3. 应用程序的执行，进入到消息循环
